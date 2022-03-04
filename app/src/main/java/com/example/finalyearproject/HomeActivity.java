42ce4b94ccdff8af36e65c81a7d424c2b4e8a37d
SarmadSaleem <sarmadawan81@gmail.com>
2022-03-04 18:34:35 +0500

    getwheels

--- app/src/main/java/com/example/finalyearproject/HomeActivity.java
+++ app/src/main/java/com/example/finalyearproject/HomeActivity.java
@@ -1,15 +1,33 @@
 package com.example.finalyearproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
     Button carrenting;
     Button carbooking;
+
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+
+    ImageView DP;
+    TextView Name_with_dp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +36,41 @@ public class HomeActivity extends AppCompatActivity {
 
         carrenting=findViewById(R.id.carrenting);
         carbooking=findViewById(R.id.carbooking);
+
+        DP=findViewById(R.id.AssignendProfile);
+        Name_with_dp=findViewById(R.id.AssignendName);
+
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView=(NavigationView)findViewById(R.id.drawer);
+        drawerLayout=(DrawerLayout)findViewById(R.id.mydrawerlayout);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.home_icon:
+                        Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.update_icon:
+                        Toast.makeText(HomeActivity.this, "Setting", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+                return true;
+            }
+        });
 
         carrenting.setOnClickListener(new View.OnClickListener() {
             @Override
