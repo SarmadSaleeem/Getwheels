6a6a818faa597ad061f93cc1161fe18603a44504
SarmadSaleem <sarmadawan81@gmail.com>
2022-03-08 20:47:55 +0500

    zz

--- app/src/main/java/com/example/finalyearproject/Driver_Registration.java
+++ app/src/main/java/com/example/finalyearproject/Driver_Registration.java
@@ -1,11 +1,36 @@
 package com.example.finalyearproject;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Driver_Registration extends AppCompatActivity {
+
+    RelativeLayout basicinfo;
+    RelativeLayout licence;
+    RelativeLayout cnic;
+    RelativeLayout vehicle_info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driver_registration);
+
+        basicinfo=findViewById(R.id.driver_basicinfo);
+        licence=findViewById(R.id.driver_driverlicence);
+        cnic=findViewById(R.id.driver_cnic);
+        vehicle_info=findViewById(R.id.driver_vehicleinfo);
+
+        basicinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent basicinfo=new Intent(Driver_Registration.this,Basic_Info_Fragment.class);
+                startActivity(basicinfo);
+            }
+        });
     }
 }
