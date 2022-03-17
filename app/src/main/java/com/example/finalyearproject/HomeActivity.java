96f9c21f7c92e597b99b726fafaa1ad38f8f3bcd
SarmadSaleem <sarmadawan81@gmail.com>
2022-03-18 00:39:16 +0500

    zz

--- app/src/main/java/com/example/finalyearproject/HomeActivity.java
+++ app/src/main/java/com/example/finalyearproject/HomeActivity.java
@@ -1,5 +1,8 @@
 package com.example.finalyearproject;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +11,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,7 +20,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class HomeActivity extends AppCompatActivity{
     Button carrenting;
@@ -26,8 +39,13 @@ public class HomeActivity extends AppCompatActivity{
     ActionBarDrawerToggle toggle;
     DrawerLayout drawerLayout;
 
-    ImageView DP;
-    TextView Name_with_dp;
+    ActivityResultLauncher<String> dp;
+
+    public Uri mydp;
+
+    FirebaseStorage firebaseStorage;
+    FirebaseDatabase firebaseDatabase;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +55,68 @@ public class HomeActivity extends AppCompatActivity{
         carrenting=findViewById(R.id.carrenting);
         carbooking=findViewById(R.id.carbooking);
 
-        DP=findViewById(R.id.AssignendProfile);
-        Name_with_dp=findViewById(R.id.AssignendName);
-
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         navigationView=(NavigationView)findViewById(R.id.drawer);
         drawerLayout=(DrawerLayout)findViewById(R.id.mydrawerlayout);
 
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        firebaseStorage=FirebaseStorage.getInstance();
+
         toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
 
         drawerLayout.addDrawerListener(toggle);
 
         toggle.syncState();
+
+        View header_view=navigationView.getHeaderView(0);
+        TextView set_name=header_view.findViewById(R.id.Assign_Name);
+        ImageView set_dp=header_view.findViewById(R.id.Assign_Profile);
+
+        dp=registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                set_dp.setImageURI(result);
+                mydp=result;
+
+                firebaseStorage.getReference("Users").child(firebaseAuth.getCurrentUser().getUid()).child("DP").putFile(mydp).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        firebaseStorage.getReference("Users").child(firebaseAuth.getCurrentUser().getUid()).child("DP")
+                                .getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                firebaseDatabase.getReference("Users").child(firebaseAuth.getCurrentUser().getUid())
+                                        .child("DP Uri").setValue(uri.toString());
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        set_dp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dp.launch("image/*");
+            }
+        });
+
+        firebaseDatabase.getReference("Users").child(firebaseAuth.getCurrentUser().getUid())
+                .child("DP Uri").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String image=snapshot.getValue(String.class);
+                Picasso.get().load(image).into(set_dp);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -70,5 +136,6 @@ public class HomeActivity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
+        
     }
 }
